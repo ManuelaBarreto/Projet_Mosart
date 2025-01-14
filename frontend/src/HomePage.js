08ccacb5db4c0be38965f49e69cb997f22a13926
MBarreto <manuelabarreto2303@gmail.com>
2025-01-14 16:17:12 +0100

    home_page

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -5,14 +5,19 @@ import './HomePage.css';
 export default function HomePage() {
   return (
     <div className="home-page">
-      <Nav_Bar variant="show-logo"/>
+      <Nav_Bar variant="" />
       <div className="content">
         <h1 className="title kaushan-script-regular">Mos'Art</h1>
-        <p className="description">
+        <p className="description inter-regular">
           Explorez le monde des mosaïques photographiques. Participez à
           l'aventure en contribuant à des créations uniques et collectives.
         </p>
-        <button className="gallery-button">Galerie</button>
+      </div>
+      <div className="footer">
+        <button className="gallery-button inter-regular">
+          <p>Galery</p>
+          <img className="DownArrow" src={require('./Assets/DownArrow.png')} alt="Down Arrow" />
+        </button>
       </div>
     </div>
   );
