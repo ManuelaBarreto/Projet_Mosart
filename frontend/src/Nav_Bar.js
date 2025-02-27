9e5dc7d0d11ff333161e2d2df4791bf7be00bdd0
MBarreto <manuelabarreto2303@gmail.com>
2025-02-27 16:44:28 +0100

    Update: Navbar icons

--- frontend/src/Nav_Bar.js
+++ frontend/src/Nav_Bar.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Nav_Bar.css';
 
-export default function Nav_Bar({ variant }) {
+export default function Nav_Bar({ variant, scrollToHome }) {
   return (
     <header className={variant === 'show-logo' ? 'nav-bar show-logo' : 'nav-bar'}>
       {variant === 'show-logo' &&
         <div className="nav-text kaushan-script-regular">Mos'Art</div>
       }
         <div className="nav-icons">
-          <span className="icon">🏠</span>
-          <span className="icon">👤</span>
+          <img className="icon"
+                src='/home.png'
+                alt="Home Icon"
+                loading="lazy"
+                onClick={scrollToHome}
+          >
+          </img>
         </div>
     </header>
   );
