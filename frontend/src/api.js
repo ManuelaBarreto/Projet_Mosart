ebf64689b9ae413f9e96276137c57dde73b0c8fe
Valentin Billa <valentin.billa@gmail.com>
2025-03-13 01:32:48 +0100

    :bug: Fix url formatting

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,21 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export async function fetchMosaics() {
-    return fetch(API_URL + 'mosaics').then(res => res.json());
+    return fetch(`${API_URL}/mosaics`).then(res => res.json());
 }
 
 export async function fetchMosaic(slug) {
-    return fetch(API_URL + 'mosaics/' + slug).then(res => res.json());
+    return fetch(`${API_URL}/mosaics/${slug}`).then(res => res.json());
 }
 
 export function getMosaicOriginalUrl(slug) {
-    return API_URL + 'mosaics/' + slug + '/original';
+    return `${API_URL}/mosaics/${slug}/original`;
 }
 
 export function getMosaicThumbnailUrl(slug) {
-    return API_URL + 'mosaics/' + slug + '/thumbnail';
+    return `${API_URL}/mosaics/${slug}/thumbnail`;
 }
 
 export function getMosaicTileUrl(slug, zoom, x, y) {
-    return API_URL + 'mosaics/' + slug + '/tiles/' + zoom + '/' + x + '_' + y;
+    return `${API_URL}/mosaics/${slug}/tiles/${zoom}/${x}_${y}`;
 }
