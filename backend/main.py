6136e0dca6feb08eb5aa35bed8d9e095a286d478
Valentin Billa <valentin.billa@gmail.com>
2025-03-13 08:35:38 +0100

    :sparkles: Adds a skip on incomplete data

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,8 @@ DATABASE_FILE = "data.json"
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000", # Frontend development server
-    "https://mosaics.vbilla.fr", # Frontend production server
+    "http://localhost:3000",  # Frontend development server
+    "https://mosaics.vbilla.fr",  # Frontend production server
     "https://api.mosaics.vbilla.fr",  # Backend API domain
 ]
 
@@ -28,6 +28,13 @@ app.add_middleware(
 def read_root():
     return {"status": "UP"}
 
+
+@app.get("/refresh")
+async def refresh_cache():
+    gather_all_details.cache_clear()
+    return
+
+
 @app.get("/mosaics")
 async def get_mosaics(clear_cache: bool = Query(default=False, alias='clear-cache')):
     if clear_cache:
@@ -40,12 +47,14 @@ async def get_mosaics(clear_cache: bool = Query(default=False, alias='clear-cach
 def gather_all_details():
     mosaics = []
     for slug in os.listdir("mosaics"):
-        path = os.path.join("mosaics", slug, "details.json")
+        details = os.path.join("mosaics", slug, "details.json")
+        thumbnail = os.path.join("mosaics", slug, "thumbnail.webp")
 
-        if not os.path.exists(path):
+        # Skips images that have yet to be generated.
+        if not os.path.exists(details) or not os.path.exists(thumbnail):
             continue
 
-        with open(path) as f:
+        with open(details) as f:
             mosaics.append(json.load(f))
     return mosaics
 
@@ -60,6 +69,7 @@ async def get_mosaic(slug: str):
     with open(path) as f:
         return json.load(f)
 
+
 @app.get("/mosaics/{slug}/original")
 async def serve_image(slug: str):
     path = os.path.join("mosaics", slug, "original.webp")
@@ -69,6 +79,7 @@ async def serve_image(slug: str):
 
     return FileResponse(path)
 
+
 @app.get("/mosaics/{slug}/thumbnail")
 async def serve_image(slug: str):
     path = os.path.join("mosaics", slug, "thumbnail.webp")
@@ -77,6 +88,7 @@ async def serve_image(slug: str):
         raise HTTPException(status_code=404, detail="Thumbnail not found")
 
     return FileResponse(path)
+
 
 @app.get("/mosaics/{slug}/tiles/{zoom}/{x}_{y}")
 async def serve_image(slug: str, zoom: int, x: int, y: int):
