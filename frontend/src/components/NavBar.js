aa97a6186ed18ccef8aeca712343369fca002181
Valentin Billa <valentin.billa@gmail.com>
2025-03-13 03:49:36 +0100

    :lipstick: Updated icons

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,7 +5,10 @@ import {Link} from "react-router-dom";
 export default function NavBar() {
   return (
     <header className="nav-bar">
-      <Link className="nav-link kaushan-script-regular" to='/'>Mos'Art</Link>
+      <Link className="nav-link" to='/'><span className="nav-text">Mos'Art</span></Link>
+      <Link className="nav-link" to='/'>
+          <img className="nav-icon" src={require("assets/home.png") ?? ""} alt="Home" />
+      </Link>
     </header>
   );
 }
